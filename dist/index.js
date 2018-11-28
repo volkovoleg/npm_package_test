fdad0946daebe0aa5e67338dd81462adb368df53
Волков Олег Евгеньевич <volkov.o.e@yandex.ru>
2018-11-28 15:38:56 +0300

    tsc

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hello_1 = require("./hello");
+exports.sayHello = hello_1.sayHello;
+exports.sayGoodbye = hello_1.sayGoodbye;
